03eabe19b2d4b8c522aaef16032dfcb9e1e79594
engineer325 <engineerlife325@gmail.com>
2021-11-06 07:52:10 +1000

    hide panel on desktop view

--- packages/pancake-uikit/src/widgets/Menu/config.ts
+++ packages/pancake-uikit/src/widgets/Menu/config.ts
@@ -205,4 +205,4 @@ export const MENU_HEIGHT = 90;
 export const SUBNAV_LINK_HEIGHT = 36;
 export const MENU_ENTRY_HEIGHT = 48;
 export const SIDEBAR_WIDTH_FULL = 240;
-export const SIDEBAR_WIDTH_REDUCED = 56;
+export const SIDEBAR_WIDTH_REDUCED = 0;
